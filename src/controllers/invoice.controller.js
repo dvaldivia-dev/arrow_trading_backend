9fbe77d4b08300611a51601a62d3a238891e5f48
Daniel_Valdivia <danyel.valo@gmail.com>
2025-10-31 17:38:38 -0600

    refactor(invoice.controller): Ajuste a los nombres de los campos para obtener la lista parcial. feat(invoice.controller): se agrego metodo para obtener todos los campos de la tabla de invoice. feat(invoice.controller): Se agregfaron 2 metodos nuevos para obtener el pdf original en formato string base 64 y otro para obtener los adjuntos de la factura, de igual forma en formato string base 64. feat(invoice.routes): se agregaron 3 rutas para los metodos que se agregaron para: getFullInvoiceList, getOriginalInvoicePDF y getAttachmentsInvoicePDF

--- src/controllers/invoice.controller.js
+++ src/controllers/invoice.controller.js
@@ -1,6 +1,8 @@
 import pool from "../config/db.js";
 import dayjs from "dayjs";
 import dotenv from "dotenv";
+import fs from "fs";
+import path from "path";
 
 dotenv.config();
 
@@ -34,22 +36,22 @@ export const getInvoiceList = async (req, res) => {
     // Consulta dinámica con conversión de fecha y filtrado
     const query = `
       SELECT 
-        Id,
-        Consecutivo,
-        Num,
+        Id as id,
+        Num as invoiceNo,
         S0Num,
-        STR_TO_DATE(IssueDate, '%m/%d/%Y') AS IssueDate,
-        BillTo,
-        ShipTo,
+        IssueDate as issueDate,
+        BillTo as billTo,
+        ShipTo as shipTo,
         lncotenn, 
-        ItemQty,
-        PriceEach,
-        Total
+        ItemQty as itemQty,
+        PriceEach as priceEach,
+        Total As total,
+        needs_review as needsReview
       FROM ATC.Invoices
       WHERE 
-        STR_TO_DATE(IssueDate, '%m/%d/%Y') BETWEEN ? AND ?
+        IssueDate BETWEEN ? AND ?
         AND BillTo IS NOT NULL
-      ORDER BY STR_TO_DATE(IssueDate, '%m/%d/%Y') DESC
+      ORDER BY IssueDate DESC
       LIMIT ?, ?;
     `;
 
@@ -67,6 +69,154 @@ export const getInvoiceList = async (req, res) => {
     });
   } catch (error) {
     console.error("Error en getInvoiceList:", error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getFullInvoiceList = async (req, res) => {
+  const { startDate, endDate, numberOfItems, offset } = req.body;
+
+  try {
+    // Validaciones básicas
+    const parsedOffset = parseInt(offset, 10) || 0;
+    const parsedLimit = parseInt(numberOfItems, 10) || 30;
+
+    if (!startDate || !endDate) {
+      return res.status(400).json({
+        message:
+          "Debes proporcionar 'startDate' y 'endDate' en el cuerpo de la solicitud.",
+      });
+    }
+
+    if (isNaN(parsedLimit) || parsedLimit <= 0) {
+      return res.status(400).json({
+        message: "El parámetro 'limit' debe ser un número entero positivo.",
+      });
+    }
+
+    if (isNaN(parsedOffset) || parsedOffset < 0) {
+      return res.status(400).json({
+        message: "El parámetro 'offset' debe ser un número entero no negativo.",
+      });
+    }
+
+    // Consulta dinámica con conversión de fecha y filtrado
+    const query = `
+      SELECT 
+        Id as id,
+        Num,
+        S0Num,
+        IssueDate as issueDate,
+        ShipDate as shipDate,
+        DueDate as dueDate,
+        BillTo as billTo,
+        ShipTo as shipTo,
+        MethodOfShipment as method,
+        PaymentTerms as terms,
+        Notes as notes,
+        lncotenn,
+        ProductNo as productNo,
+        Description as description,
+        Amount as amount,
+        UM as um,
+        ItemQty as itemQty,
+        PriceEach as priceEach,
+        PriceOriginal as priceOriginal,
+        Subtotal as subtotal,
+        Total as total,
+        needs_review as needsReview
+      FROM ATC.Invoices
+      WHERE 
+        IssueDate BETWEEN ? AND ?
+        AND BillTo IS NOT NULL
+      ORDER BY IssueDate DESC
+      LIMIT ?, ?;
+    `;
+
+    const [rows] = await pool.query(query, [
+      startDate,
+      endDate,
+      parsedOffset,
+      parsedLimit,
+    ]);
+
+    res.json({
+      list: rows,
+      result: rows.length > 0,
+      total: rows.length,
+    });
+  } catch (error) {
+    console.error("Error en getInvoiceList:", error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getOriginalInvoicePDF = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const [rows] = await pool.query(
+      "SELECT OriginalPDFPath FROM Invoices WHERE Id = ?",
+      [id]
+    );
+
+    if (rows.length === 0) {
+      return res.status(404).json({ message: "Factura no encontrada." });
+    }
+
+    const filePath = rows[0].OriginalPDFPath;
+
+    if (!filePath) {
+      return res
+        .status(404)
+        .json({ message: "La factura no tiene un PDF original asociado." });
+    }
+
+    if (fs.existsSync(filePath)) {
+      const fileBuffer = fs.readFileSync(filePath);
+      const base64String = fileBuffer.toString("base64");
+      res.json({ pdfBase64: base64String });
+    } else {
+      res.status(404).json({ message: "Archivo PDF original no encontrado." });
+    }
+  } catch (error) {
+    console.error("Error en getOriginalInvoicePDF:", error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getAttachmentsInvoicePDF = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const [rows] = await pool.query(
+      "SELECT AttachmentsPDFPath FROM Invoices WHERE Id = ?",
+      [id]
+    );
+
+    if (rows.length === 0) {
+      return res.status(404).json({ message: "Factura no encontrada." });
+    }
+
+    const filePath = rows[0].AttachmentsPDFPath;
+
+    if (!filePath) {
+      return res
+        .status(404)
+        .json({ message: "La factura no tiene un PDF de adjuntos asociado." });
+    }
+
+    if (fs.existsSync(filePath)) {
+      const fileBuffer = fs.readFileSync(filePath);
+      const base64String = fileBuffer.toString("base64");
+      res.json({ pdfBase64: base64String });
+    } else {
+      res
+        .status(404)
+        .json({ message: "Archivo PDF de adjuntos no encontrado." });
+    }
+  } catch (error) {
+    console.error("Error en getAttachmentsInvoicePDF:", error);
     res.status(500).json({ error: error.message });
   }
 };
--- src/routes/invoice.routes.js
+++ src/routes/invoice.routes.js
@@ -1,8 +1,16 @@
 import { Router } from "express";
-import { getInvoiceList } from "../controllers/invoice.controller.js";
+import {
+  getInvoiceList,
+  getFullInvoiceList,
+  getOriginalInvoicePDF,
+  getAttachmentsInvoicePDF,
+} from "../controllers/invoice.controller.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = Router();
 router.post("/invoices", verifyToken, getInvoiceList);
+router.post("/invoices/full", verifyToken, getFullInvoiceList);
+router.get("/invoices/original/:id", getOriginalInvoicePDF);
+router.get("/invoices/attachments/:id", getAttachmentsInvoicePDF);
 
 export default router;
